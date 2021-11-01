b9c2b40cb57d8e5b2633ffd1019dc9791cf66969
張庭瑜 <tingyuchang960625@gmail.com>
2021-11-01 21:54:24 +0800

    新增855033作業

---
+++ 855033/彗星列車.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    int S;
+    cin >> A;
+    cin >> S;
+    if(S/A==0){
+        cout << S/A;
+    } else {
+        cout << S/A+1;
+    }
+}
---
+++ 855033/電梯.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std ;
+
+int main() {
+    int n;
+    cin >> n;
+    int last_F=1;
+    int sum_power = 0;
+    for (int i = 0; i < n; i++) {
+        int floor;
+        cin >> floor;
+        int power;
+        if(floor - last_F > 0) {
+            power = (floor - last_F)*3;
+        } else if(floor - last_F < 0) {
+            power = (last_F -floor )*2;
+        } else {
+            power = 0;
+        }
+        sum_power = sum_power + power;
+        last_F = floor;
+    }
+    cout << sum_power;
+}
